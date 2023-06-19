808804202a916ee327392a503866f559bce02a0c
scentre <ememe.ebuka@imalipay.com>
2023-06-19 22:37:57 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import { App } from './components/app';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { App } from "./components/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>
+  <Provider store={store}>
+    <Router>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Router>
+  </Provider>
 );
-
-reportWebVitals();
